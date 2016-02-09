49126b2f5358a81189271a5d6a8829681a58a4f0
Sandy Chapman <sandychapman@gmail.com>
2016-02-09 14:40:53 -0400

    Add example for validation fail on POST.

--- api/mocks/hello_world.js
+++ api/mocks/hello_world.js
@@ -3,9 +3,14 @@
 var util = require('util');
 
 module.exports = {
-  hello: hello
+  hello: hello,
+  post_hello: post_hello
 };
 
 function hello(req, res) {
+  res.json(["abc", "abbbbc", "abbbbbbbc"]);
+}
+
+function post_hello(req, res) {
   res.json(["abc", "abbbbc", "abbbbbbbc"]);
 }
